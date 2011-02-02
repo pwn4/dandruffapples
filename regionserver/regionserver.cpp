265259cac7b331b9cdcb8fc4baab8f99371fc25c
Peter Neufeld <pwn4@sfu.ca>
2011-02-02 01:10:49 -0800

    First almost-empty file for regionserver

---
+++ regionserver/regionserver.cpp
@@ -0,0 +1,20 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////////////
+Regionserver program
+This program communications with clients, controllers, PNGviewers, other regionservers, and clockservers.
+//////////////////////////////////////////////////////////////////////////////////////////////////*/
+#include <stdio.h>
+
+
+int main()
+{
+	//Print a starting message
+	printf("--== Region Server Software ==-\n");
+	
+	printf("Server Initializing ...\n");
+	
+	printf("Server Running!\n");
+	
+	printf("Server Shutting Down ...\n");
+	
+	printf("Goodbye!\n");
+}
