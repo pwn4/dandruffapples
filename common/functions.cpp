4299f35583bda42e0eefce9724d9094866a63fde
Peter Neufeld <pwn4@sfu.ca>
2011-02-03 20:34:24 -0800

    workhorse functions library. Contains parsePacket

---
+++ common/functions.cpp
@@ -0,0 +1,58 @@
+//This library contains a set of workhorse functions and their required objects for all entities.
+#include "functions.h"
+
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cerrno>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+//takes a string with packetBuffer data, returns packetType=-1 if no packet data in the string
+//otherwise returns the google proto object and its type (for casting).
+//WARNING: modifies the string given!
+protoPacket parsePacket(std::string * packetBuffer)
+{
+    protoPacket rtn;
+    
+    std::string token;
+    size_t nextDelim;
+    int packetsize;
+    
+    //we may have multiple packets all in the buffer together. Tokenize one. Damn TCP streaming
+    nextDelim = (*packetBuffer).find('\0');
+    if(nextDelim == string::npos)
+    {
+        rtn.packetType = -1;
+        return rtn;
+    }
+
+    //the first string is the proto id.
+    token = (*packetBuffer).substr(0, nextDelim);
+    rtn.packetType = atoi(token.c_str());
+    (*packetBuffer).erase(0, nextDelim+1); //take it off
+
+    //the next string is the packet length
+    nextDelim = (*packetBuffer).find('\0');
+    token = (*packetBuffer).substr(0, nextDelim);
+    packetsize = atoi(token.c_str());
+    (*packetBuffer).erase(0, nextDelim+1); //take it off
+    
+    //we now have our packet. normally use the packettype to identify it, but this is just for now
+    rtn.packetData = (*packetBuffer).substr(0, packetsize);
+    (*packetBuffer).erase(0, rtn.packetData.length()); //take it off
+    ///////////////////////
+    
+    return rtn;
+    
+}
