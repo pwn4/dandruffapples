561c2dcd415d8ab8975af2f7dd7a8c4959896dd0
Benjamin Saunders <besaunde@sfu.ca>
2011-02-04 14:20:16 -0800

    Add user-specifiable prefix messages to SystemErrors.

--- common/except.h
+++ common/except.h
@@ -1,12 +1,15 @@
 #include <stdexcept>
 #include <cerrno>
 #include <cstring>
+#include <string>
 
 // TODO: Store errno
 class SystemError : public std::runtime_error {
  public:
   SystemError() : runtime_error(strerror(errno)) {}
+  SystemError(const std::string &msg) : runtime_error((msg + ": ") + strerror(errno)) {}
   SystemError(int errno_) : runtime_error(strerror(errno_)) {}
+  SystemError(int errno_, const std::string &msg) : runtime_error((msg + ": ") + strerror(errno_)) {}
 };
 
 class EOFError : public std::runtime_error {
