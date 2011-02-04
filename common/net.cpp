5e709442f74046b8d042fd19d5c4c84245053437
Peter Neufeld <pwn4@sfu.ca>
2011-02-04 01:18:07 -0800

    moved listen code to net class. Now uses net::do_listen(int port) to create listening socket for that port

--- common/net.cpp
+++ common/net.cpp
@@ -19,10 +19,45 @@
 using namespace std;
 
 namespace net {
+    int do_listen(int port) {
+
+        int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if(0 > sock) {
+            perror("Failed to create socket");
+            return sock;
+        }
+
+        int yes = 1;
+        if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("Failed to reuse existing socket");
+            return sock;
+        }
+
+        struct sockaddr_in clockaddr;
+        memset(&clockaddr, 0, sizeof(struct sockaddr_in));
+        clockaddr.sin_family = AF_INET;
+        clockaddr.sin_port = htons(port);
+        clockaddr.sin_addr.s_addr = INADDR_ANY;
+
+        if(0 > bind(sock, (struct sockaddr *)&clockaddr, sizeof(struct sockaddr_in))) {
+            perror("Failed to bind socket");
+            close(sock);
+            return -1;
+        }
+
+        if(0 > listen(sock, 1)) {
+            perror("Failed to listen on socket");
+            close(sock);
+            return -1;
+        }
+        
+        return sock;
+    }
+
   int do_connect(const char *address, int port) {
     int fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(0 > fd) {
-     return fd;
+        return fd;
     }
 
     // Set non-blocking
