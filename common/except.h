f852612e5fc8eabee776d2cd7faf3a0394069ec1
Benjamin Saunders <besaunde@sfu.ca>
2011-02-04 17:05:18 -0800

    Store errno value in SystemErrors.

--- common/except.h
+++ common/except.h
@@ -5,14 +5,18 @@
 
 // TODO: Store errno
 class SystemError : public std::runtime_error {
- public:
-  SystemError() : runtime_error(strerror(errno)) {}
-  SystemError(const std::string &msg) : runtime_error((msg + ": ") + strerror(errno)) {}
-  SystemError(int errno_) : runtime_error(strerror(errno_)) {}
-  SystemError(int errno_, const std::string &msg) : runtime_error((msg + ": ") + strerror(errno_)) {}
+protected:
+  int _number;
+public:
+  SystemError() : runtime_error(strerror(errno)), _number(errno) {}
+  SystemError(const std::string &msg) : runtime_error((msg + ": ") + strerror(errno)), _number(errno) {}
+  SystemError(int errno_) : runtime_error(strerror(errno_)), _number(errno_) {}
+  SystemError(int errno_, const std::string &msg) : runtime_error((msg + ": ") + strerror(errno_)), _number(errno_) {}
+
+  int number() { return _number;}
 };
 
 class EOFError : public std::runtime_error {
- public:
+public:
   EOFError() : runtime_error("End of file") {}
 };
