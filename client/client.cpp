f80c2d2b3914fb51bf6387b84b78f196f4e9bedd
Peter Neufeld <pwn4@sfu.ca>
2011-02-04 17:33:12 -0800

    client addition

---
+++ client/client.cpp
@@ -0,0 +1,183 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////////////
+Client program
+This program communications with controllers.
+//////////////////////////////////////////////////////////////////////////////////////////////////*/
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cerrno>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../common/ports.h"
+#include "../common/timestep.pb.h"
+#include "../common/net.h"
+#include "../common/messagewriter.h"
+#include "../common/messagereader.h"
+#include "../common/except.h"
+
+using namespace std;
+
+/////////////////Variables and Declarations/////////////////
+char configFileName [30] = "config";
+
+//Config variables
+char clockip [40] = "127.0.0.1";
+
+////////////////////////////////////////////////////////////
+
+
+//this function parses any minimal command line arguments and uses their values
+void parseArguments(int argc, char* argv[])
+{
+	//loop through the arguments
+	for(int i = 0; i < argc; i++)
+	{
+		//if it's a configuration file name...
+		if(strcmp(argv[i], "-c") == 0)
+		{
+			strcpy(configFileName, argv[i+1]);
+		
+			printf("Using config file: %s\n", configFileName);
+			
+			i++; //increment the loop counter for one argument
+		}
+	}
+}
+
+
+//this function loads the config file so that the server parameters don't need to be added every time
+void loadConfigFile()
+{
+	//open the config file
+	FILE * fileHandle;
+	fileHandle = fopen (configFileName,"r");
+	
+	//create a read buffer. No line should be longer than 200 chars long.
+	char readBuffer [200];
+	char * token;
+	
+	if (fileHandle != NULL)
+	{
+		while(fgets (readBuffer , sizeof(readBuffer) , fileHandle) != 0)
+		{	
+			token = strtok(readBuffer, " \n");
+			
+			//if it's a REGION WIDTH definition...
+			if(strcmp(token, "CLOCKIP") == 0){
+				token = strtok(NULL, " \n");
+				strcpy(clockip, token);
+				printf("Using clockserver IP: %s\n", clockip);
+			}
+			
+		}
+		
+		fclose (fileHandle);
+	}else
+		printf("Error: Cannot open config file %s\n", configFileName);
+}
+
+char *parse_port(char *input) {
+  size_t input_len = strlen(input);
+  char *port;
+  
+  for(port = input; *port != ':' && (port - input) < input_len; ++port);
+  
+  if((port - input) == input_len) {
+    return NULL;
+  } else {
+    // Split the string
+    *port = '\0';
+    ++port;
+    // Strip newline
+    char *end;
+    for(end = port; *end != '\n'; ++end);
+    if(end == port) {
+      return NULL;
+    } else {
+      *end = '\0';
+      return port;
+    }
+  }
+}
+
+void run() {
+  cout << "Connecting to clock server..." << flush;
+  int clockfd = net::do_connect(clockip, CLOCK_PORT);
+  if(0 > clockfd) {
+    perror(" failed to connect to clock server");
+    return;
+  } else if(0 == clockfd) {
+    cerr << " invalid address: " << clockip << endl;
+    return;
+  }
+
+  cout << " done." << endl;
+
+  TimestepUpdate timestep;
+  TimestepDone tsdone;
+  tsdone.set_done(true);
+  MessageWriter writer(clockfd, TIMESTEPDONE, &tsdone);
+  MessageReader reader(clockfd);
+  MessageType type;
+  size_t len;
+  const void *buffer;
+
+  try {
+    cout << "Notifying clock server that our init's complete..." << flush;
+    while(true) {
+      for(bool complete = false; !complete;) {
+        complete = writer.doWrite();
+      }
+      cout << " done." << endl;
+      
+      cout << "Waiting for timestep..." << flush;
+      for(bool complete = false; !complete;) {
+        complete = reader.doRead(&type, &len, &buffer);
+      }
+      timestep.ParseFromArray(buffer, len);
+      cout << " got " << timestep.timestep() << ", replying..." << flush;
+    }
+  } catch(EOFError e) {
+    cout << " clock server disconnected, shutting down." << endl;
+  } catch(SystemError e) {
+    cerr << " error performing network I/O: " << e.what() << endl;
+  }
+
+  // Clean up
+  shutdown(clockfd, SHUT_RDWR);
+  close(clockfd);
+}
+
+//this is the main loop for the server
+int main(int argc, char* argv[])
+{
+	//Print a starting message
+	printf("--== Client Software ==-\n");
+	
+	////////////////////////////////////////////////////
+	printf("Server Initializing ...\n");
+	
+	parseArguments(argc, argv);
+	
+	loadConfigFile();
+	////////////////////////////////////////////////////
+	
+	printf("Server Running!\n");
+	
+	run();
+	
+	printf("Server Shutting Down ...\n");
+	
+	printf("Goodbye!\n");
+}
