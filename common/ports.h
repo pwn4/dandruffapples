1f73cfe75964d97fa33d8463a7ab44a067a7fa9b
Peter Neufeld <pwn4@sfu.ca>
2011-02-06 22:51:25 -0800

    added regions listening port default

--- common/ports.h
+++ common/ports.h
@@ -1,9 +1,11 @@
 #ifndef _PORTS_H_
 #define _PORTS_H_
 
+//only default port numbers
 #define CLOCK_PORT 50000
 #define CONTROLLERS_PORT 50010
 #define CLIENTS_PORT 50020
 #define PNG_VIEWER_PORT 50040
+#define REGIONS_PORT 50050
 
 #endif
