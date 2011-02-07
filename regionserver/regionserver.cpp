d0fcd44c4fc6211d50d71eeebd6f9be3cb8df962
Peter Neufeld <pwn4@sfu.ca>
2011-02-06 19:55:22 -0800

    just added some libraries

--- regionserver/regionserver.cpp
+++ regionserver/regionserver.cpp
@@ -2,33 +2,40 @@
 Regionserver program
 This program communications with clients, controllers, PNGviewers, other regionservers, and clockservers.
 //////////////////////////////////////////////////////////////////////////////////////////////////*/
+#include <sstream>
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <cerrno>
+#include <tr1/memory>
 
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/fcntl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/select.h>
+#include <sys/epoll.h>
 
 #include <stdio.h>
 #include <string>
 #include <string.h>
 #include <stdlib.h>
 
-#include "../common/ports.h"
+#include <google/protobuf/message_lite.h>
+
 #include "../common/timestep.pb.h"
 #include "../common/net.h"
 #include "../common/serverrobot.pb.h"
 #include "../common/puckstack.pb.h"
 #include "../common/messagewriter.h"
-#include "../common/messagereader.h"
-#include "../common/except.h"
+#include "../common/worldinfo.pb.h"
 
-#include "../common/helper.h"
+
+#include "../common/ports.h"
+#include "../common/messagereader.h"
+#include "../common/messagequeue.h"
+#include "../common/net.h"
+#include "../common/except.h"
 
 using namespace std;
 
