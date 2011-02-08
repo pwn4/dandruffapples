22cd8d6467794c54f555ddc945f22362055073a7
hoboman313 <hoboman313@hotmail.com>
2011-02-07 18:25:46 -0800

    Part 1: creating the logmerger directory

---
+++ logmerger/logmerger.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "../common/helper.h"
+#include "../common/messagewriter.h"
+#include "../common/messagereader.h"
+
+#include "../common/timestep.pb.h"
+#include "../common/serverrobot.pb.h"
+#include "../common/puckstack.pb.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	helper::Config config(argc, argv);
+
+	return 0;
+}
