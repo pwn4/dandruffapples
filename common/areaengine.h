6d2781f25eaed40c93807296c0e778d4feee05cb
Peter Neufeld <pwn4@sfu.ca>
2011-02-10 17:45:13 -0800

    added array creation to the constructor

--- common/areaengine.h
+++ common/areaengine.h
@@ -1,12 +1,16 @@
 #ifndef _AREAENGINE_H_
 #define _AREAENGINE_H_
 
+
 class AreaEngine {
 protected:
-  
+int topx, topy;   //displacement of the region. allows us to use absolute coords everywhere
+int robotRatio, regionRatio;    //robotDiameter:puckDiameter, regionSideLength:puckDiameter
+int** puckArray;
+int** robotArray;
   
 public:
-  AreaEngine();
+  AreaEngine(int xdisp, int ydisp, int robotSize, int regionSize, int maxMemory);
   ~AreaEngine();
 
 };
