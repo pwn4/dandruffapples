ceaa17f2783f7cee654b61a201f055fde2d86efe
Peter Neufeld <pwn4@sfu.ca>
2011-02-10 16:44:51 -0800

    uncommented logging

--- regionserver/regionserver.cpp
+++ regionserver/regionserver.cpp
@@ -374,7 +374,7 @@ void run() {
                 }
               }
 
-              /*logWriter.init(MSG_TIMESTEPUPDATE, timestep);
+              logWriter.init(MSG_TIMESTEPUPDATE, timestep);
               logWriter.doWrite();
 
               for(int i = 0;i <50; i++) {
@@ -389,7 +389,7 @@ void run() {
             	  for(bool complete = false; !complete;) {
             	    complete = logWriter.doWrite();;
             	  }
-               }*/
+               }
 
               //Respond with done message
               msg_ptr update(new TimestepDone(tsdone));
