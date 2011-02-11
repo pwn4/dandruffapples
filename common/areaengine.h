a511c42f0fee5fdd5f28490aea621bac3b2c0b83
Peter Neufeld <pwn4@sfu.ca>
2011-02-10 19:07:18 -0800

    Started adding working objects and functions

--- common/areaengine.h
+++ common/areaengine.h
@@ -1,16 +1,50 @@
 #ifndef _AREAENGINE_H_
 #define _AREAENGINE_H_
 
+#define TOP_LEFT 0
+#define TOP 1
+#define TOP_RIGHT 2
+#define RIGHT 3
+#define BOTTOM_RIGHT 4
+#define BOTTOM 5
+#define BOTTOM_LEFT 6
+#define LEFT 7
+
+struct Index{
+  int x, y;
+  
+  Index() : x(0), y(0) {}
+  Index(int newx, int newy) : x(newx), y(newy) {}
+};
+
+struct PuckStackObject{
+  int x, y;
+  int count;
+  
+  PuckStackObject * nextStack;
+};
+
+struct RobotObject{
+  double x, y;
+  double vx, vy;
+  
+  RobotObject * nextRobot;
+  
+  RobotObject(double newx, double newy) : x(newx), y(newy), vx(0), vy(0) {}
+  RobotObject(double newx, double newy, double newvx, double newvy) : x(newx), y(newy), vx(newvx), vy(newvy) {}
+};
 
 class AreaEngine {
 protected:
-int topx, topy;   //displacement of the region. allows us to use absolute coords everywhere
 int robotRatio, regionRatio;    //robotDiameter:puckDiameter, regionSideLength:puckDiameter
+int regionBounds;
 int** puckArray;
 int** robotArray;
   
 public:
-  AreaEngine(int xdisp, int ydisp, int robotSize, int regionSize, int maxMemory);
+  Index getRobotIndices(double x, double y);
+  RobotObject AddRobot(double newx, double newy, double newvx, double newvy);
+  AreaEngine(int robotSize, int regionSize, int maxMemory);
   ~AreaEngine();
 
 };
