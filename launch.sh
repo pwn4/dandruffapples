8373d6e11270eef0d4cfa17662648ade300312ea
Peter Neufeld <pwn4@sfu.ca>
2011-03-02 01:28:54 -0800

    actually, make the log in the same directory that you start the clock server in.

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ SSHPORT=24
 # Path of project dir relative to $HOME
 PROJDIR="dandruffapples"
 # Path to write generated clock config to
-CLOCKCONF="~/tmp/antix-clock-conf"
+CLOCKCONF="antix-clock-conf"
 # Path to read CSIL hosts from
 HOSTFILE="./hosts"
 # Number of regions to launch on a single machine
