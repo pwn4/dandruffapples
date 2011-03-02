352f27ba5c1ba441a23f88c21375c4289066e9c5
Peter Neufeld <pwn4@sfu.ca>
2011-03-01 20:51:56 -0800

    robots larger than 1 pixel are now gradient colored dots

--- worldviewer/drawer.cpp
+++ worldviewer/drawer.cpp
@@ -31,8 +31,12 @@ TwoInt ByteUnpack(unsigned int data) {
   return TwoInt(first, second);
 
 }
+
+//globals
 ColorObject coloringMap[65535];
 bool colorMapInitialized = false;
+cairo_pattern_t *radpat;  //for drawing gradients with large robots
+
 //this is the color mapping method: teams->Color components
 ColorObject colorFromTeam(int teamId){
   //initialize the color map for the first time
@@ -90,18 +94,19 @@ cairo_surface_t * UnpackImage(RegionRender render, int robotSize, double robotAl
 
     //pixel precision robot drawing
     if(robotSize == 1){
-      cairo_set_source_rgb(stepImageDrawer, robotColor.r, robotColor.g, robotColor.b);
+      cairo_set_source_rgba(stepImageDrawer, robotColor.r, robotColor.g, robotColor.b, robotAlpha);
       cairo_rectangle (stepImageDrawer, curRobot.one, curY, robotSize, robotSize);
       cairo_fill (stepImageDrawer);
     }else{
     //aliased precision
-      cairo_set_source_rgba(stepImageDrawer, robotColor.r, robotColor.g, robotColor.b, robotAlpha);
-      cairo_set_line_width(stepImageDrawer, robotSize);
-      cairo_set_line_cap(stepImageDrawer, CAIRO_LINE_CAP_ROUND);
-
-      cairo_move_to(stepImageDrawer, curRobot.one, curY);  
-      cairo_line_to(stepImageDrawer, curRobot.one, curY);
-      cairo_stroke(stepImageDrawer);
+        //init the gradient
+      radpat = cairo_pattern_create_radial (curRobot.one, curY, 0.0,  curRobot.one, curY, (double)robotSize / 2.0);
+      cairo_pattern_add_color_stop_rgba (radpat, 0,  robotColor.r, robotColor.g, robotColor.b, robotAlpha);
+      cairo_pattern_add_color_stop_rgba (radpat, (double)robotSize / 2.0,  robotColor.r, robotColor.g, robotColor.b, 0.0);
+      
+      cairo_rectangle (stepImageDrawer, curRobot.one - (robotSize / 2.0), curY - (robotSize / 2.0), robotSize, robotSize);
+      cairo_set_source (stepImageDrawer, radpat);
+      cairo_fill (stepImageDrawer);
     }
     
   }
