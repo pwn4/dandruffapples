c9166634a7f3b251a313487e9bdada1804ef5111
Benjamin Saunders <besaunde@sfu.ca>
2011-03-22 04:54:32 -0700

    Scrapped MSS tweaking until we get flushing.

--- client/client.cpp
+++ client/client.cpp
@@ -766,7 +766,7 @@ gboolean Client::run(GIOChannel *ioch, GIOCondition cond, gpointer data) {
 	}
 
   int mss = net::get_mss(g_io_channel_unix_get_fd(ioch));
-  if(!writing && (!gotTeam || controller.queue.remaining() >= (mss - (0.1*mss)))) {
+  if(!writing && controller.queue.remaining()) {
     g_source_remove(gwatch);
     // Ensure that we're watching for writability
     gwatch = g_io_add_watch(ioch, (GIOCondition)(G_IO_IN | G_IO_OUT), runner, data);
