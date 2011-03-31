9176a053b559f30b1f582132b53f45668892ee05
Youyou <superyoyoy@gmail.com>
2011-03-31 11:34:26 -0700

    AI with less clustering

--- client/ai_friendly_vaughan.cpp
+++ client/ai_friendly_vaughan.cpp
@@ -34,18 +34,26 @@ private:
 		// we don't have destinations
 		// TODO: maybe choose a puck that's closest to our home as the next destination?
 		// for now, randomly move
-		pair<double, double> v = randomMove(command, defaultSpeed);
+		pair<double, double> v = randomMove(command, defaultSpeed, 0, 360);
 
 		pair<double, double> newdest = make_pair(50 * v.first-ownRobot->homeRelX, 50 * v.second-ownRobot->homeRelY);
 		ownRobot->destPush(newdest);
 	}
 
+	// random move: default rotate angle would be 90~270 to avoid collision
 	pair<double, double> randomMove(ClientRobotCommand& command, double speed){
-		pair<double, double> v = polar2cartesian(speed, degree2radian(rand()%360));
+		pair<double, double> v = polar2cartesian(speed, degree2radian(rand()%180+90));
 		command.setVx(v.first);
 		command.setVy(v.second);
 		return v;
 	}
+
+	pair<double, double> randomMove(ClientRobotCommand& command, double speed,int angleS, int angleE){
+			pair<double, double> v = polar2cartesian(speed, degree2radian(rand()%(angleE-angleS)+angleS));
+			command.setVx(v.first);
+			command.setVy(v.second);
+			return v;
+		}
 
 	bool noSpeed(OwnRobot* ownRobot) {
 		return abs(ownRobot->vx) < 0.0001 && abs(ownRobot->vy) < 0.0001;
@@ -140,8 +148,8 @@ public:
 		if (closestRobot != NULL) {
 			double dist = relDistance(closestRobot->relx, closestRobot->rely);
 			double m = relDistance(command.vx, command.vy);
-			if (dist < 10 * m) {
-				m = dist / 10;
+			if (dist < 5 * m) {
+				m = dist / 5;
 				pair<double, double> v = make_pair(command.vx, command.vy);
 				normalize(v, m);
 				command.setVx(v.first);
@@ -152,7 +160,6 @@ public:
 
 	void decide(ClientRobotCommand& command, OwnRobot* ownRobot) {
 		// SMARTER AI
-		//
 		/*
 		 *  If robot in home and has no puck
 		 * 		go up
@@ -169,13 +176,15 @@ public:
 		 */
 
 		if (!ownRobot->hasPuck) {
-			if (relDistance(ownRobot->vx, ownRobot->vy) < 4 * HOMEDIAMETER && ownRobot->vy < 0) {
-				// go up when robot is inside home and has dropped puck
-				if (noSpeed(ownRobot)) {
+			// go up when robot is close to home and has no puck
+			if (relDistance(ownRobot->homeRelX, ownRobot->homeRelY) < 4 * HOMEDIAMETER
+					&& (-1)*ownRobot->homeRelY < 0.7*HOMEDIAMETER) {
+				if (noSpeed(ownRobot) && (-1)*ownRobot->homeRelY < 0.5*HOMEDIAMETER) {
 					randomMove(command, 0.2);
 				} else {
-					command.setVy(-1);
-					if (ownRobot->vy < -0.2 * HOMEDIAMETER) {
+					command.setVy(-2);
+					// if still inside home area, go outwards
+					if ((-1)*ownRobot->homeRelY < -0.1 * HOMEDIAMETER) {
 						pair<double, double> v = make_pair((-1)*ownRobot->homeRelX, (-1)*ownRobot->homeRelY);
 						normalize(v, defaultSpeed);
 						command.setVx(v.first);
@@ -186,7 +195,9 @@ public:
 				}
 				return;
 			}
+			// try if can pickup puck
 			SeenPuck* pickup = findPickUpablePuck(ownRobot);
+			// yes we can
 			if (pickup != NULL) {
 				// We're on a puck, we think! Set velocity to 0 and pickup.
 				command.setPuckPickup(true);
@@ -195,16 +206,16 @@ public:
 				ownRobot->focus = false;
 				return;
 			} else {
+			// no we can't pickup puck
 				if (ownRobot->focus) {
-					// do not change destination
+					// do not change destination, when go with focus
 				} else {
-					// go to the next nearest puck
-
+					// locate the nearest puck
 					SeenPuck* closestPuck = findClosestPuck(ownRobot);
 					if (closestPuck != NULL && !ownRobot->focus) {
 						pair<double, double> newdest(closestPuck->relx-ownRobot->homeRelX,
 								closestPuck->rely-ownRobot->homeRelY);
-						// push the next destination
+						// go towards it!
 						ownRobot->destPush(newdest);
 						ownRobot->focus = true;
 						//TODO push all seen pucks to the known puck list
@@ -213,15 +224,15 @@ public:
 				nextDestination(command, ownRobot);
 			}
 		} else {
-		// has puck
+		// we have a puck
 			if (relDistance(ownRobot->homeRelX, ownRobot->homeRelY)
 					< HOMEDIAMETER / 2.2) {
-				// Robot at home, drop puck
+				// Robot at home, drop puck, score!
 				command.setPuckPickup(false);
 				ownRobot->has_path_home = false;
 				ownRobot->focus = false;
 				command.setVx(0.0);
-				command.setVy(0.5);
+				command.setVy(-0.5);
 				ownRobot->destPopFront();
 			} else {
 			// Robot outside home
@@ -231,8 +242,12 @@ public:
 					ownRobot->has_path_home = true;
 				}
 			}
+
+			// Robot on its way to home
 			if (!ownRobot->focus) {
-				if ((-1)*ownRobot->homeRelY > 0.2 * HOMEDIAMETER && (-1)*ownRobot->homeRelY < 4 * HOMEDIAMETER) {
+				// if ready to go towards home, go with focus
+				if ((-1)*ownRobot->homeRelY > 0.3 * HOMEDIAMETER
+				 && (-1)*ownRobot->homeRelY < 2.5 * HOMEDIAMETER) {
 					ownRobot->clearDestinations();
 					pair<double, double> v = make_pair(0, 0);
 					ownRobot->destPush(v);
@@ -240,58 +255,49 @@ public:
 				}
 				nextDestination(command, ownRobot);
 			} else {
+				// Going directly to home
 				// manually guide robot to home
+				if ((-1)*ownRobot->homeRelX < -0.3 * HOMEDIAMETER) {
+					// go right
+					command.setVy(0);
+					command.setVx(2);
+				} else if ((-1)* ownRobot->homeRelX > 0.3 * HOMEDIAMETER ) {
+					// go left
+					command.setVy(0);
+					command.setVx(-2);
+				} else {
+					// go up
+					command.setVy(-1);
+					command.setVx(0);
+				}
 
-					if ((-1)*ownRobot->homeRelX < -0.3 * HOMEDIAMETER) {
-						// go right
-						command.setVy(0);
-						command.setVx(2);
-					} else if ((-1)* ownRobot->homeRelX > 0.3 * HOMEDIAMETER ) {
-						// go left
-						command.setVy(0);
-						command.setVx(-2);
-					} else {
-						// go up
-						command.setVy(-1);
-						command.setVx(0);
-					}
-
-					SeenRobot* closestRobot = findClosestRobot(ownRobot);
-					if (closestRobot) {
-						double dist = relDistance(closestRobot->relx, closestRobot->rely);
-						if (dist < 1.5*ROBOTDIAMETER) {
-							command.setVx(command.vx/50);
-							command.setVy(command.vy/50);
-							int d = rand() % 100;
-							if (d > 50) {
-								randomMove(command, 0.15);
+				SeenRobot* closestRobot = findClosestRobot(ownRobot);
+				if (closestRobot) {
+					double dist = relDistance(closestRobot->relx, closestRobot->rely);
+					if (dist < 2*ROBOTDIAMETER) {
+						pair<double, double> v = make_pair(command.vx, command.vy);
+						command.setVx(command.vx/50);
+						command.setVy(command.vy/50);
+						int d = rand() % 100;
+						if (d > 35) {
+							randomMove(command, 0.1, 0, 360);
+							//randomMove(command, 0.1);
+							double dh = relDistance(ownRobot->homeRelX, ownRobot->homeRelY);
+							if (dh < 0.6 * HOMEDIAMETER + 2 * ROBOTDIAMETER) {
+								randomMove(command, 0.15, 0, 360);
+								//randomMove(command, 0.15);
 							}
 						}
-
 					}
 
-				return;
-				/*
-				nextDestination(command, ownRobot);
-				if (relDistance(ownRobot->homeRelX, ownRobot->homeRelY) < 5 * HOMEDIAMETER) {
-
-
-					// we're stuck on the way to home, and we're close to home
-					if (ownRobot->vx < 0.0001 && ownRobot->vy < 0.0001) {
-						// randomly wait
-						randomMove(command);
-						//ownRobot->has_path_home = false;
-					}
-					return;
 				}
-				*/
+				return;
 			}
 
 		}
 
-		// it's good except when we collides
-		if (abs(ownRobot->vx) < 0.0001 && abs(ownRobot->vy) < 0.0001) {
-
+		// it's good except when we collide
+		if (noSpeed(ownRobot)) {
 			if (ownRobot->hasDestination()) {
 				// it have target, turn to right for 60 degrees
 				pair<double, double> top = ownRobot->destFront();
@@ -304,230 +310,8 @@ public:
 				command.setVx(newv.first);
 				command.setVy(newv.second);
 			} else {
-				// go wherever
-				double velocity = defaultSpeed;
-				double modx = 1.0;
-				double mody = 1.0;
-				//move away!
-				if (rand() % 3 == 0) {
-					// Move right!
-					command.setVx(velocity * -1.0 * modx);
-				} else {
-					command.setVx(velocity * modx);
-				}
-				if (rand() % 4 == 0) {
-					// Move down!
-					command.setVy(velocity * -1.0 * mody);
-				} else {
-					command.setVy(velocity * mody);
-				}
-			}
-
-		}
-
-		return;
-
-		if (!ownRobot->hasPuck) {
-			SeenPuck* pickup = findPickUpablePuck(ownRobot);
-			if (pickup != NULL && !insideOurHome(ownRobot)) {
-				// We're on a puck, we think! Set velocity to 0 and pickup.
-				command.setPuckPickup(true);
-				command.setVx(0.0);
-				command.setVy(0.0);
-			} else {
-				// Make robot move in direction of the nearest puck.
-				SeenPuck* closest = findClosestPuck(ownRobot);
-				SeenRobot* closestRobot = findClosestRobot(ownRobot);
-				if (closest != NULL) {
-					// We can see at least one puck!
-					if (closest->rely == 0) //prevent NaN
-						closest->rely = 0.000001;
-					double ratio = abs(closest->relx / closest->rely);
-					double modx = 1.0;
-					double mody = 1.0;
-					if (ratio > 1.0) {
-						mody = 1.0 / ratio;
-					} else {
-						modx = ratio;
-					}
-
-					if (closestRobot == NULL || (closestRobot != NULL
-							&& (relDistance(closestRobot->relx,
-									closestRobot->rely) > 1.5 * ROBOTDIAMETER))) {
-						//noone is close to us
-						double velocity = 1.0;
-						if (relDistance(closest->relx, closest->rely) < 5.0) {
-							// The closer we are to the puck, the slower we move!
-							velocity = 0.1;
-						}
-						if (closest->relx <= 0.0) {
-							// Move left!
-							command.setVx(velocity * -1.0 * modx);
-						} else if (closest->relx > 0.0) {
-							command.setVx(velocity * modx);
-						}
-						if (closest->rely <= 0.0) {
-							// Move up!
-							command.setVy(velocity * -1.0 * mody);
-						} else if (closest->rely > 0.0) {
-							command.setVy(velocity * mody);
-						}
-					} else {
-						//some other robot is closer to the puck, go look for another
-						SeenPuck* secondClosest = findSecondClosestPuck(
-								ownRobot);
-						if (secondClosest != NULL) {
-							// We can see at least one puck!
-							if (secondClosest->rely == 0) //prevent NaN
-								secondClosest->rely = 0.000001;
-							double ratio = abs(
-									secondClosest->relx / secondClosest->rely);
-							double modx = 1.0;
-							double mody = 1.0;
-							if (ratio > 1.0) {
-								mody = 1.0 / ratio;
-							} else {
-								modx = ratio;
-							}
-
-							double velocity = 1.0;
-							if (relDistance(closest->relx, closest->rely) < 5.0) {
-								// The closer we are to the puck, the slower we move!
-								velocity = 0.1;
-							}
-							if (closest->relx <= 0.0) {
-								// Move left!
-								command.setVx(velocity * -1.0 * modx);
-							} else if (closest->relx > 0.0) {
-								command.setVx(velocity * modx);
-							}
-							if (closest->rely <= 0.0) {
-								// Move up!
-								command.setVy(velocity * -1.0 * mody);
-							} else if (closest->rely > 0.0) {
-								command.setVy(velocity * mody);
-							}
-
-						} else {
-							//or backoff from it
-							double velocity = ((rand() % 11) / 10.0);
-							;
-							//move away!
-							if (closestRobot->relx > 0.0) {
-								// Move right!
-								command.setVx(velocity * -1.0 * modx);
-							} else {
-								command.setVx(velocity * modx);
-							}
-							if (closestRobot->rely > 0.0) {
-								// Move down!
-								command.setVy(velocity * -1.0 * mody);
-							} else {
-								command.setVy(velocity * mody);
-							}
-						}
-					}
-				} else {
-					// We can't see any pucks! Move if we are "not moving".
-					if (abs(ownRobot->vx) < 0.3 && abs(ownRobot->vy) < 0.3) {
-						// We're moving too slow! New random velocity.
-						double velocity = 1.0;
-						double modx = 1.0;
-						double mody = 1.0;
-						//move away!
-						if (rand() % 3 == 0) {
-							// Move right!
-							command.setVx(velocity * -1.0 * modx);
-						} else {
-							command.setVx(velocity * modx);
-						}
-						if (rand() % 4 == 0) {
-							// Move down!
-							command.setVy(velocity * -1.0 * mody);
-						} else {
-							command.setVy(velocity * mody);
-						}
-					}
-				}
-			}
-		} else if (ownRobot->hasPuck) {
-			// We have a puck!
-			//do /3.5 to ensure we're really inside the home before dropping. None of this border crap.
-			if (relDistance(ownRobot->homeRelX, ownRobot->homeRelY)
-					< HOMEDIAMETER / 2.2) {
-				// We're home! Drop the puck.
-				command.setPuckPickup(false);
-				command.setVx(0.0);
-				command.setVy(0.0);
-			} else {
-				// Carry the puck home!
-				SeenRobot* closestRobot = findClosestRobot(ownRobot);
-				if (ownRobot->homeRelY == 0) //prevent NaN
-					ownRobot->homeRelY = 0.000001;
-				double ratio = abs(ownRobot->homeRelX / ownRobot->homeRelY);
-				double modx = 1.0;
-				double mody = 1.0;
-				if (ratio > 1.0) {
-					mody = 1.0 / ratio;
-				} else {
-					modx = ratio;
-				}
-				if (closestRobot == NULL || (closestRobot != NULL
-						&& (relDistance(closestRobot->relx, closestRobot->rely)
-								> 1.5 * ROBOTDIAMETER))) {
-					double velocity = 1.0;
-					if (ownRobot->homeRelX <= 0.0) {
-						// Move left!
-						command.setVx(velocity * -1.0 * modx);
-					} else if (ownRobot->homeRelX > 0.0) {
-						command.setVx(velocity * modx);
-					}
-					if (ownRobot->homeRelY <= 0.0) {
-						// Move up!
-						command.setVy(velocity * -1.0 * mody);
-					} else if (ownRobot->homeRelY > 0.0) {
-						command.setVy(velocity * mody);
-					}
-				} else if ((closestRobot != NULL && (relDistance(
-						closestRobot->relx, closestRobot->rely) < 1.5
-						* ROBOTDIAMETER))) {
-					//something is blocking us...try to slightly differenty but in same direction
-					int randNum = rand();
-					double velocity = ((randNum % 11) / 10.0);
-					if (randNum % 4 > 1) {
-						if (ownRobot->homeRelX <= 0.0 && randNum % 5 > 1) {
-							// Move left!
-							command.setVx(velocity * -1.0 * modx);
-						} else {
-							command.setVx(velocity * modx);
-						}
-					}
-					if (randNum % 4 > 2) {
-						if (ownRobot->homeRelY <= 0.0 && randNum % 5 > 1) {
-							// Move up!
-							command.setVy(velocity * -1.0 * mody);
-						} else {
-							command.setVy(velocity * mody);
-						}
-					}
-				}
-			}
-		} else if (insideOurHome(ownRobot)) {
-			//get out of the house!
-			double velocity = 1.0;
-			double modx = 1.0;
-			double mody = 1.0;
-			if (ownRobot->homeRelX < HOMEDIAMETER / 2) {
-				// Move left!
-				command.setVx(velocity * -1.0 * modx);
-			} else if (ownRobot->homeRelX >= HOMEDIAMETER / 2) {
-				command.setVx(velocity * modx);
-			}
-			if (ownRobot->homeRelY < HOMEDIAMETER / 2) {
-				// Move up!
-				command.setVy(velocity * -1.0 * mody);
-			} else if (ownRobot->homeRelY >= HOMEDIAMETER / 2) {
-				command.setVy(velocity * mody);
+				// random move
+				randomMove(command, defaultSpeed);
 			}
 		}
 		return;
